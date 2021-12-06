e0e1bad623304596ae7c85732576cf370684f01e
Courtney Jensen <courtney@prologmobile.com>
2021-12-06 07:50:19 -0800

    updated the ability to see waves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,13 +6,53 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+
+    /*
+     * A little magic, Google what events are in Solidity!
+     */
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    /*
+     * I created a struct here named Wave.
+     * A struct is basically a custom datatype where we can customize what we want to hold inside it.
+     */
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
+
+    /*
+     * I declare a variable waves that lets me store an array of structs.
+     * This is what lets me hold all the waves anyone ever sends to me!
+     */
+    Wave[] waves;
+
     constructor() {
         console.log("I think I am a smart contract, but I can not think.. so am I really smart?");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s has waved! They must think you are pretty neat!", msg.sender);
+        console.log("%s has waved! They must think you are pretty neat!", msg.sender, _message);
+
+        /*
+         * This is where I actually store the wave data in the array.
+         */
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         * I added some fanciness here
+         */
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    /*
+     * I added a function getAllWaves which will return the struct array, waves, to us.
+     * This will make it easy to retrieve the waves from our website!
+     */
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
--- scripts/run.js
+++ scripts/run.js
@@ -1,34 +1,35 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-  
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
-  
-    let totalWaves;
-    totalWaves = await waveContract.getTotalWaves();
-    
-    let newWave = await waveContract.wave();
-    await newWave.wait();
-  
-    totalWaves = await waveContract.getTotalWaves();
-    
-    newWave = await waveContract.connect(randomPerson).wave();
-    await newWave.wait();
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log('Contract address:', waveContract.address);
 
-    totalWaves = await waveContract.getTotalWaves();
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-  runMain();
+  /**
+   * Let's send a few waves!
+   */
+  let waveTxn = await waveContract.wave('This wave may look small within the digital ocean, but remember the digital ocean does not exist without these waves.');
+  await waveTxn.wait(); // Wait for the transaction to be mined
+
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave('Our waves combined will change the tide!');
+  await waveTxn.wait(); // Wait for the transaction to be mined
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
